53dbf4065c7f75f2a60ae389b5e0675690342289
= <=>
2025-02-23 19:16:23 -0300

    exercise 2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,22 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast/Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isToastOpen, setIsToastOpen] = React.useState(false);
+
+  const showToast = () => {
+    setIsToastOpen(true);
+  };
+
+  const dismissToast = () => {
+    setIsToastOpen(false);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +26,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isToastOpen && (
+        <Toast handleDismiss={dismissToast} variant={variant}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -60,7 +76,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={showToast}>Pop Toast!</Button>
           </div>
         </div>
       </div>
